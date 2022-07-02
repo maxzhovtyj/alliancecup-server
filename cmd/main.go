419f764b6ffa7812bbab4467cef8c9ee8272cb2f
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2022-07-02 12:20:00 +0300

    db postgres add

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,26 @@ import (
 	"allincecup-server/pkg/handler"
 	"allincecup-server/pkg/repository"
 	"allincecup-server/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(server.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
