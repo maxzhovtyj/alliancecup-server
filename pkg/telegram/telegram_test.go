edbbf5779689a6cc61fab3306a8aa5b1fc20e70a
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2023-07-22 14:32:19 +0300

    fix tg test

--- pkg/telegram/telegram_test.go
+++ pkg/telegram/telegram_test.go
@@ -3,10 +3,10 @@ package telegram
 import "testing"
 
 func TestSendMessage(t *testing.T) {
-	//Init("6310276967:AAHbN2bbQDx0q05_wY4liIoxaudFc6ZGYI4", -824380677)
-	//
-	//err := Send("Hello Everybody!!!")
-	//if err != nil {
-	//	t.Fatal(err)
-	//}
+	m := NewManager("", 0)
+
+	err := m.Send("Hello World!")
+	if err != nil {
+		t.Fatal(err)
+	}
 }
